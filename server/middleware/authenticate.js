8389532aaf048539d39e1182e1d523ad4c325856
Shek Yu Hong <shek_yu_hong@tech.gov.sg>
2018-09-19 16:59:02 +0800

    Added missing return in options check

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -13,11 +13,14 @@ authMiddlewareRouter.use((req, res, next) => {
 const checkTokenValidity = (req, res, next) => {
     console.log('req.method', req.method);
     if (req.method === 'OPTIONS') {
+        console.log('Skipping Token Check for OPTIONS');
         next();
+        return;
     }
 
     let accessToken = req.headers.authorization ? req.headers.authorization.split(' ')[1] : '';
     if (accessToken === '') {
+        console.log('Missing token');
         res.status(401);
         res.json({ error: 'Missing token' });
         return;
