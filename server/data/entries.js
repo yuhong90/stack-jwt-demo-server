8f28188ed0c965201ec12368c14c5c76b2e56ff2
Shek Yu Hong <shek_yu_hong@tech.gov.sg>
2018-09-28 00:15:17 +0800

    Revamped riddle to amusement park theme

---
+++ server/data/entries.js
@@ -0,0 +1,3 @@
+const entriesList = {};
+
+module.exports = entriesList;
---
+++ server/routes/park.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const entriesList = require('../data/entries');
+const authMiddlewareRouter = require('../middleware/authenticate');
+
+const parkRouter = express.Router();
+
+parkRouter.get('/entries', function (req, res) {
+    let data = { entries: entriesList };
+    res.status(200);
+    res.send(data);
+});
+
+parkRouter.use(authMiddlewareRouter);
+
+parkRouter.post('/entries', function (req, res) {
+    if (!req.session.sub || !req.session.jti) {
+        res.status(400);
+        res.send({ error: 'Missing sub or jti' });
+        return;
+    }
+
+    let entry = { name: req.session.sub };
+    entriesList[req.session.jti] = entry;
+    res.status(200);
+    res.send({ message: 'Welcome to STACK Amusement Park!' });
+});
+
+module.exports = parkRouter;
